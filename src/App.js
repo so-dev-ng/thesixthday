fa007ce3710bcd0bc001b83511d17b8b05ef034d
so-dev-ng <factful@gmail.com>
2021-03-16 23:53:31 +0900

    complete scroll

--- src/App.js
+++ src/App.js
@@ -1,26 +1,54 @@
 import './App.css';
 import axios from 'axios';
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 
 function App() {
   const [items, setItems] = useState([]);
+  const [pages, setPages] = useState(0);
 
-  const loadItems = async () => {
-    const newItems = (await axios.get('http://dev.dolobox.co/api/get/items/2')).data.data;
-    setItems(items.concat(newItems))
-    console.log(newItems);
+  const loadMoreItems = async () => {
+    const newItems = (await axios.get(`http://dev.dolobox.co/api/get/items/${pages}`)).data.data;
+    setItems(items.concat(newItems));
+    setPages(pages + 1);
   }
   //컨텐트 불러오기 <div dangerouslySetInnerHTML={{ __html: item.contents }}></div>
+
+  const loadItems = async () => {
+    const scrollHeight = document.documentElement.scrollHeight;
+    const scrollTop = document.documentElement.scrollTop;
+    const clientHeight = document.documentElement.clientHeight;
+
+    if (scrollTop + clientHeight >= scrollHeight) loadMoreItems();
+  }
+  useEffect(() => {
+    loadMoreItems();
+    // eslint-disable-next-line 
+  }, [])
+
+  // useEffect(() => {
+  //   const firstLoad = async () => {
+  //     const newItems = (await axios.get('http://dev.dolobox.co/api/get/items/0')).data.data;
+  //     setItems(items.concat(newItems));
+  //     setPages(pages + 1);
+  //   }
+  //   firstLoad();
+  // }, [])
+
+  useEffect(() => {
+    window.addEventListener('scroll', loadItems);
+    return () => {
+      window.removeEventListener('scroll', loadItems);
+    }
+  })
 
   return (
     <div className="App">
       <header className="App-header">
-        <button onClick={loadItems}>불러오기</button>
-        {items.map((item) => (
-          <>
-            <div>{item.thumb_small_img}</div>
-            <div>{item.title}</div>
-          </>
+        {items.map((item, idx) => (
+          <div className="container" key={idx}>
+            <div className="img">{item.thumb_small_img}</div>
+            <div className="title">{item.title}</div>
+          </div>
         ))}
       </header>
     </div>
