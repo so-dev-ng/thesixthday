de673634922deec8e3be6ac5bf944fe80e78bc88
so-dev-ng <factful@gmail.com>
2021-03-16 21:45:38 +0900

    load first data

--- src/App.js
+++ src/App.js
@@ -1,20 +1,27 @@
 import './App.css';
+import axios from 'axios';
+import { useState } from 'react'
 
 function App() {
+  const [items, setItems] = useState([]);
+
+  const loadItems = async () => {
+    const newItems = (await axios.get('http://dev.dolobox.co/api/get/items/2')).data.data;
+    setItems(items.concat(newItems))
+    console.log(newItems);
+  }
+  //컨텐트 불러오기 <div dangerouslySetInnerHTML={{ __html: item.contents }}></div>
+
   return (
     <div className="App">
       <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <button onClick={loadItems}>불러오기</button>
+        {items.map((item) => (
+          <>
+            <div>{item.thumb_small_img}</div>
+            <div>{item.title}</div>
+          </>
+        ))}
       </header>
     </div>
   );
