ebdfb24ecf1cbdc195ae749ea3155735c9a7166b
so-dev-ng <factful@gmail.com>
2021-03-17 21:16:01 +0900

    :recycle: delete unnecessary action

--- src/module.js
+++ src/module.js
@@ -1,10 +1,8 @@
 //액션 타입
 const SET_ITEMS = 'SET_ITEMS';
-const NEXT_PAGE = 'NEXT_PAGE';
 
 //액션 생성 함수
 export const setItems = item => ({ type: SET_ITEMS, item });
-export const nextPage = () => ({ type: NEXT_PAGE });
 
 //초기 상태
 const initialState = {
@@ -18,11 +16,7 @@ export default function reducer(state = initialState, action) {
     case SET_ITEMS:
       return {
         ...state,
-        items: state.items.concat(action.item)
-      };
-    case NEXT_PAGE:
-      return {
-        ...state,
+        items: state.items.concat(action.item),
         pages: state.pages + 1
       };
     default:
